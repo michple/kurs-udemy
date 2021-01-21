f19b72c51ad3315da5ad14903668a126aa4b3f5f
Michal_Plewa <michaple1997@gmail.com>
2021-01-21 11:31:54 +0100

    webpack konfiguracja i dodatki

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -3,15 +3,12 @@ import 'lazysizes'
 import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
 import StickyHeader from './modules/StickyHeader'
-// import Modal from './modules/Modal'
 
-// new Modal();
 new StickyHeader();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
 new MobileMenu();
 let modal;
-
 
 document.querySelectorAll(".open-modal").forEach(el => {
     el.addEventListener("click", e => {
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
 const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const fse = require('fs-extra')
 
 const postCSSPlugins = [
     require('postcss-import'),
@@ -6,40 +10,90 @@ const postCSSPlugins = [
     require('postcss-simple-vars'),
     require('postcss-nested'),
     require('postcss-hexrgba'),
-    require('autoprefixer')
+    require('autoprefixer'),
+    require('cssnano') //kompresja css
 ]
+class RunAfterCompile {
+    apply(compiler) {
+        compiler.hooks.done.tap('copy Images', function() {
+            fse.copySync('./app/assets/images', './docs/assets/images')
+        })
+    }
+}
+// new HtmlWebpackPlugin({ // stworzenie wyściowego htmla razem z importami plików w folderze dist
+//        filename:'index.html',
+//        template:'./app/index.html'
+//    }),
+
+let pages = fse.readdirSync('./app').filter(function(file) {
+    return file.endsWith('.html')
+}).map(function(page) {
+    return new HtmlWebpackPlugin({
+        filename: page,
+        template: `./app/${page}`
+    })
+});
+pages.push(new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({ // wyciągnięcie css z js
+        filename: 'style.[chunkhash].css'
+    }),
+    new RunAfterCompile())
 
 module.exports = {
 
     entry: './app/assets/scripts/app.js',
-    output: {
-        filename: 'bundled.js',
-        path: path.resolve(__dirname, 'app')
-    },
-    devServer: { //ustawienie autoodświeżania
-        historyApiFallback: true,
-        compress: true,
-        before: function(app, server) {
-            server._watch('./app/**/*.html')
-        },
-        contentBase: path.join(__dirname, 'app'), // jaki folder ma być sprawdzany
-        hot: true,
-        port: 3000,
-        host: '192.168.0.103' // widoczność strony w sieci lan
-    },
 
-    mode: 'development',
+    output: {
+        filename: '[name].[chunkhash].js',
+        chunkFilename: '[name].[chunkhash].js',
+        path: path.resolve(__dirname, 'docs')
+    },
+    optimization: {
+        splitChunks: { chunks: 'all' }
+    },
+    plugins: pages,
+    mode: 'production',
     module: {
         rules: [{
             test: /\.css$/i,
             use: [
-                'style-loader', 'css-loader?url=false', { loader: "postcss-loader", options: { postcssOptions: { plugins: postCSSPlugins } } }
+                MiniCssExtractPlugin.loader, 'css-loader?url=false', { loader: "postcss-loader", options: { postcssOptions: { plugins: postCSSPlugins } } }
             ]
+        }, {
+            test: /\.js$/,
+            exclude: /(node_modules)/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
         }]
     },
 
 }
+// module.rules.push({
+//     test: /\.js$/,
+//     exclude: /(node_modules)/,
+//     use: {
+//         loader: 'babel-loader',
+//         options: {
+//             presets: ['@babel/preset-env']
+//         }
+//     }
+// })
 
+// devServer:{ //ustawienie autoodświeżania
+//     historyApiFallback: true,
+//     compress: true,
+//     before: function(app,server) {
+//         server._watch('./app/**/*.html')
+//     },
+//     contentBase: path.join(__dirname,'app'), // jaki folder ma być sprawdzany
+//     hot:true,
+//     port:3000,
+//     host: '192.168.0.103' // widoczność strony w sieci lan
+// },
 /*
 {loader: 'postcss-loader', options: {plugins: postCSSPlugins}}
 
